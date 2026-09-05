a10aff9947559e89e9498b3d5722c867616803cf
Gnu-Gorets <164275518+Gnu-Gorets@users.noreply.github.com>
2026-09-05 16:41:42 +0200

    android: add FFmpegKit dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,4 +39,5 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.documentfile:documentfile:1.0.1")
+    implementation("dev.ffmpegkit-maintained:ffmpeg-kit-min-gpl:8.1.7")
 }
