43789e3f9c64efb90fc7ca91b0b029709c766b48
Gnu-Gorets <164275518+Gnu-Gorets@users.noreply.github.com>
2026-09-06 05:22:21 +0200

    refactor(android): replace legacy Android wrapper

--- src/renderer/components/GeneralSettings/GeneralSettings.vue
+++ src/renderer/components/GeneralSettings/GeneralSettings.vue
@@ -16,7 +16,6 @@
           :default-value="backendFallback"
           :compact="true"
           :tooltip="t('Tooltips.General Settings.Fallback to Non-Preferred Backend on Failure')"
-          :disabled="usingBrowser"
           @change="updateBackendFallback"
         />
         <FtToggleSwitch
@@ -54,13 +53,6 @@
           :default-value="hideToTrayOnMinimize"
           :compact="true"
           @change="updateHideToTrayOnMinimize"
-        />
-        <FtToggleSwitch
-          v-if="USING_ANDROID"
-          :label="t('General Settings.Show Tap Highlight')"
-          :default-value="showTapHighlight"
-          :compact="true"
-          @change="updateShowTapHighlight"
         />
       </div>
     </div>
@@ -203,8 +195,7 @@ const currentInvidiousInstanceInputRef = useTemplateRef('currentInvidiousInstanc
 const USING_ELECTRON = !!process.env.IS_ELECTRON
 const SUPPORTS_LOCAL_API = !!process.env.SUPPORTS_LOCAL_API
 const IS_MAC = process.platform === 'darwin'
-const USING_BROWSER = !process.env.IS_ELECTRON && !process.env.IS_ANDROID
-const USING_ANDROID = process.env.IS_ANDROID
+
 const { t } = useI18n()
 const router = useRouter()
 
@@ -257,12 +248,6 @@ const hideToTrayOnMinimize = computed(() => store.getters.getHideToTrayOnMinimiz
  */
 function updateHideToTrayOnMinimize(value) {
   store.dispatch('updateHideToTrayOnMinimize', value)
-}
-
-const showTapHighlight = computed(() => store.getters.getTapHighlight)
-
-function updateShowTapHighlight(value) {
-  store.dispatch('updateTapHighlight', value)
 }
 
 /** @type {import('vue').ComputedRef<boolean>} */
