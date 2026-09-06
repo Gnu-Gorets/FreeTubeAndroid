9c001d83b66570a3e0497a50113abbf8cb1663c4
Gnu-Gorets <164275518+Gnu-Gorets@users.noreply.github.com>
2026-09-06 05:22:21 +0200

    refactor(android): replace legacy Android wrapper

--- src/renderer/views/Settings/Settings.vue
+++ src/renderer/views/Settings/Settings.vue
@@ -232,30 +232,9 @@ function handleUnlock() {
   })
 }
 
-const currentPath = ref(window.location.hash)
-
-function onPopState(_, forcedMobile = true) {
-  currentPath.value = window.location.hash
-  if (currentPath.value === '#/settings') {
-    returnToSettingsMenu()
-  } else {
-    const subPath = currentPath.value.split('#/settings#')[1]
-    if (subPath !== undefined) {
-      navigateToSection(subPath, forcedMobile)
-    }
-  }
-}
-
-function pushState(path) {
-  if (`#/settings${path}` !== window.location.hash) {
-    history.pushState({}, null, `#/settings${path}`)
-  }
-}
-
 onBeforeUnmount(() => {
   document.removeEventListener('scroll', markScrolledToSectionAsActive)
   window.removeEventListener('resize', handleResize)
-  window.removeEventListener('popstate', onPopState)
 })
 
 function showKeyboardShortcutPrompt() {
@@ -276,9 +255,6 @@ function handleMounted() {
 
   // mark first section as active before any scrolling has taken place
   activeSection.value = settingsSectionComponents.value[0].type
-
-  onPopState(undefined, true)
-  window.addEventListener('popstate', onPopState)
 }
 
 const sectionRefs = useTemplateRef('sectionRefs')
@@ -286,7 +262,7 @@ const sectionRefs = useTemplateRef('sectionRefs')
 /**
  * @param {string} sectionType
  */
-function navigateToSection(sectionType, forcedMobile = false) {
+function navigateToSection(sectionType) {
   if (isInDesktopView.value) {
     nextTick(() => {
       const sectionElement = sectionRefs.value.find(sectionRef => {
@@ -299,10 +275,7 @@ function navigateToSection(sectionType, forcedMobile = false) {
       sectionHeading.focus()
       sectionHeading.tabIndex = -1
     })
-  }
-
-  if ((!isInDesktopView.value) || forcedMobile) {
-    pushState(`#${sectionType}`)
+  } else {
     settingsSectionTypeOpenInMobile.value = sectionType
   }
 }
