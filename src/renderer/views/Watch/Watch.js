b17d5b45b57f207b75027062a4392069f3d41ae7
Gnu-Gorets <164275518+Gnu-Gorets@users.noreply.github.com>
2026-09-06 23:51:57 +0200

    fix(android): fall back from SABR to DASH

--- src/renderer/views/Watch/Watch.js
+++ src/renderer/views/Watch/Watch.js
@@ -1083,6 +1083,7 @@ export default defineComponent({
             })
 
             if (
+              !result.streaming_data.adaptive_formats.some(format => format.url || format.signature_cipher || format.cipher) &&
               videoInfo.info.streaming_data?.server_abr_streaming_url &&
               videoInfo.info.player_config.media_common_config.media_ustreamer_request_config
             ) {
@@ -1123,8 +1124,12 @@ export default defineComponent({
 
         if (this.activeFormat === 'legacy' && (this.isLive || this.isPostLiveDvr || this.legacyFormats.length === 0)) {
           // Legacy wanted as default but unavailable
-          showToast(this.t('Change Format.Legacy formats are not available for this video'))
-          this.handleActiveFormatUnavailable()
+          if (this.manifestSrc !== null) {
+            this.enableDashFormat()
+          } else {
+            showToast(this.t('Change Format.Legacy formats are not available for this video'))
+            this.handleActiveFormatUnavailable()
+          }
         }
 
         this.isLoading = false
