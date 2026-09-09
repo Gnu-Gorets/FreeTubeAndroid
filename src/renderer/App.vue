c50c7321e86f8a1ba6154e783228797634fe61c0
Gnu-Gorets <164275518+Gnu-Gorets@users.noreply.github.com>
2026-09-10 01:36:36 +0200

    perf(android): render app before background data loads

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -217,6 +217,8 @@ onMounted(async () => {
     }
   })
 
+  dataReady.value = true
+
   store.dispatch('grabAllProfiles', t('Profile.All Channels')).then(() => {
     store.dispatch('grabHistory')
     store.dispatch('grabAllPlaylists')
@@ -230,8 +232,6 @@ onMounted(async () => {
       enableOpenUrl()
       store.dispatch('getExternalPlayerCmdArgumentsData')
     }
-
-    dataReady.value = true
 
     setTimeout(() => {
       checkForNewUpdates()
