6a150ae14e1ebe760387e2e835376d3d1348837e
Gnu-Gorets <164275518+Gnu-Gorets@users.noreply.github.com>
2026-09-09 02:23:58 +0200

    fix(android): save downloads in Freetube directory

--- android/app/src/main/java/io/freetubeapp/freetubeandroid/AndroidBridge.kt
+++ android/app/src/main/java/io/freetubeapp/freetubeandroid/AndroidBridge.kt
@@ -354,6 +354,9 @@ class AndroidBridge(
     }
 
     @JavascriptInterface
+    fun getDownloadDirectory(): String = DownloadStorage.DEFAULT_DIRECTORY
+
+    @JavascriptInterface
     fun revokeDownloadDirectory(tree: String): Boolean {
         return try {
             revokePermissionForTree(tree)
