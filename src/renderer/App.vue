7384a4a98c3f91ceee2984958bf03e4f22323d55
Gnu-Gorets <164275518+Gnu-Gorets@users.noreply.github.com>
2026-09-10 03:15:14 +0200

    perf(android): add startup timing marks

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -173,6 +173,8 @@ function handleOpenDownloads() {
 }
 
 onMounted(async () => {
+  performance.mark('freetube:app-mounted')
+
   if (process.env.IS_ANDROID) {
     window.addEventListener('youtube-link', ({ detail }) => {
       if (detail?.link) handleYoutubeLink(detail.link)
@@ -186,6 +188,7 @@ onMounted(async () => {
     store.dispatch('fetchInvidiousInstancesFromFile')
   ])
 
+  performance.mark('freetube:critical-local-data-ready')
   updateTheme()
 
   if (defaultInvidiousInstance.value === '') {
@@ -213,6 +216,7 @@ onMounted(async () => {
     }
 
     dataReady.value = true
+    performance.mark('freetube:shell-ready')
 
     setTimeout(() => {
       checkForNewUpdates()
