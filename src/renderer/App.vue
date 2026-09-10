d2d25ece618dea313bfbfc318a646d67bac538b4
Gnu-Gorets <164275518+Gnu-Gorets@users.noreply.github.com>
2026-09-10 03:31:41 +0200

    perf(android): defer noncritical network work

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -195,12 +195,6 @@ onMounted(async () => {
     await store.dispatch('setRandomCurrentInvidiousInstance')
   }
 
-  store.dispatch('fetchInvidiousInstances').then(() => {
-    if (defaultInvidiousInstance.value === '') {
-      store.dispatch('setRandomCurrentInvidiousInstance')
-    }
-  })
-
   store.dispatch('grabAllProfiles', t('Profile.All Channels')).then(() => {
     store.dispatch('grabHistory')
     store.dispatch('grabAllPlaylists')
@@ -219,8 +213,13 @@ onMounted(async () => {
     performance.mark('freetube:shell-ready')
 
     setTimeout(() => {
+      store.dispatch('fetchInvidiousInstances').then(() => {
+        if (defaultInvidiousInstance.value === '') {
+          store.dispatch('setRandomCurrentInvidiousInstance')
+        }
+      })
       checkForNewUpdates()
-    }, 500)
+    }, 2000)
   })
 
   if (route.path === '/') {
