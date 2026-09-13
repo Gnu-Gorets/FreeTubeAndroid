ec5454f37db5bc0ae3a605e89f1312bf392b4b2d
Gnu-Gorets <164275518+Gnu-Gorets@users.noreply.github.com>
2026-09-13 16:54:34 +0200

    feat(android): add proxy settings support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,4 +35,5 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.documentfile:documentfile:1.0.1")
+    implementation("androidx.webkit:webkit:1.12.1")
 }
