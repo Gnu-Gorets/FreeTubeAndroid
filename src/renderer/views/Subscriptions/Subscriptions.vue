ebdc6c02a15ab42a3d681ef921437e00db99b033
Gnu-Gorets <164275518+Gnu-Gorets@users.noreply.github.com>
2026-09-13 03:04:22 +0200

    feat(android): add pull-to-refresh

--- src/renderer/views/Subscriptions/Subscriptions.vue
+++ src/renderer/views/Subscriptions/Subscriptions.vue
@@ -6,12 +6,12 @@
           :icon="['fas', 'rss']"
           class="subscriptionIcon"
         />
-        {{ $t("Subscriptions.Subscriptions") }}
+        {{ $t('Subscriptions.Subscriptions') }}
       </h2>
       <FtFlexBox
         class="tabs"
         role="tablist"
-        :aria-label="$t('Subscriptions.Subscriptions Tabs')"
+        :aria-label="$t('Subscriptions.Subscriptions Tabs', { missingWarn: false })"
       >
         <!-- eslint-disable-next-line vuejs-accessibility/interactive-supports-focus -->
         <div
