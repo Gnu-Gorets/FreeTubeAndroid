ee4fce7b19730dfac4071b146993be4a6e0721e2
Gnu-Gorets <164275518+Gnu-Gorets@users.noreply.github.com>
2026-09-14 23:36:32 +0200

    fix(android): stabilize coordinate-free smoke tests

--- src/renderer/components/TopNav/TopNav.vue
+++ src/renderer/components/TopNav/TopNav.vue
@@ -46,6 +46,7 @@
       <button
         v-if="!hideSearchBar"
         class="navSearchButton navButton"
+        data-test="search-open"
         :aria-label="t('Search Bar.Open Search Container')"
         :title="t('Search Bar.Open Search Container')"
         @click="toggleSearchContainer"
