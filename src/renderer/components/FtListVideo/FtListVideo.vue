314c046070170f375d60ab49d16a3812f32997fc
Gnu-Gorets <164275518+Gnu-Gorets@users.noreply.github.com>
2026-09-19 19:50:48 +0200

    fix

--- src/renderer/components/FtListVideo/FtListVideo.vue
+++ src/renderer/components/FtListVideo/FtListVideo.vue
@@ -1074,7 +1074,7 @@ async function handleExternalPlayer() {
           return bIsMp4 - aIsMp4 || (b.bitrate ?? 0) - (a.bitrate ?? 0)
         })[0]
       const useDirectAdaptiveVideo = networkType === 'mobile' && Boolean(selectedAdaptiveVideo)
-      const useAdaptiveStreams = Boolean(selectedAdaptiveVideo && selectedAdaptiveAudio)
+      const useAdaptiveStreams = !selectedFormat && Boolean(selectedAdaptiveVideo && selectedAdaptiveAudio)
       if (useAdaptiveStreams) {
         externalStreams = {
           videoUrl: formatUrl(selectedAdaptiveVideo),
