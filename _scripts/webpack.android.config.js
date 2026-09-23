6b8865427a08c4e97b10f2900e5212ae17baf5f9
Gnu-Gorets <164275518+Gnu-Gorets@users.noreply.github.com>
2026-09-23 19:23:07 +0200

    fix(android): remove obsolete Babel loader

--- _scripts/webpack.android.config.js
+++ _scripts/webpack.android.config.js
@@ -38,11 +38,6 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
